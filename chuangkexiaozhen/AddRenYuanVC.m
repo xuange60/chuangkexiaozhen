4bd0df58b3a23a3f497fcf91f8aa528b23b6d076
xiaogao <11027688199@qq.com>
2017-08-05 13:16:09 +0800

    111

---
+++ chuangkexiaozhen/AddRenYuanVC.m
@@ -0,0 +1,28 @@
+//
+//  AddRenYuanVC.m
+//  chuangkexiaozhen
+//
+//  Created by 小灰灰 on 2017/8/5.
+//  Copyright © 2017年 小灰灰. All rights reserved.
+//
+
+#import "AddRenYuanVC.h"
+
+@interface AddRenYuanVC ()
+
+@end
+
+@implementation AddRenYuanVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
