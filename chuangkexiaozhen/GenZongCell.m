8fa48db8ea20458a51750c42163ab5d30123efb7
zhangxuan <734267682@qq.com>
2017-08-05 22:41:28 +0800

    1111

---
+++ chuangkexiaozhen/GenZongCell.m
@@ -0,0 +1,24 @@
+//
+//  TuiChuCell.m
+//  chuangkexiaozhen
+//
+//  Created by zx on 2017/8/5.
+//  Copyright © 2017年 小灰灰. All rights reserved.
+//
+
+#import "GenZongCell.h"
+
+@implementation GenZongCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
