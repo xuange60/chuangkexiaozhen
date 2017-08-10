9daec369f94460da1b6d0faa288b110a4ee32bfa
xiaogao <11027688199@qq.com>
2017-08-10 17:06:57 +0800

    666666

---
+++ chuangkexiaozhen/DianZiHeTongVC.h
@@ -0,0 +1,23 @@
+//
+//  DianZiHeTongVC.h
+//  chuangkexiaozhen
+//
+//  Created by 小灰灰 on 2017/8/10.
+//  Copyright © 2017年 小灰灰. All rights reserved.
+//
+
+#import "BaseViewController.h"
+#import "FilelistViewController.h"
+@interface DianZiHeTongVC : BaseViewController
+
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+
+
+
+
+
+
+
+@end
---
+++ chuangkexiaozhen/DianZiHeTongVC.m
@@ -0,0 +1,51 @@
+//
+//  DianZiHeTongVC.m
+//  chuangkexiaozhen
+//
+//  Created by 小灰灰 on 2017/8/10.
+//  Copyright © 2017年 小灰灰. All rights reserved.
+//
+
+#import "DianZiHeTongVC.h"
+
+@interface DianZiHeTongVC ()
+
+@end
+
+@implementation DianZiHeTongVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  
+    
+}
+
+
+
+
+
+
+
+
+
+
+- (IBAction)detailBtnClicked:(id)sender forEvent:(UIEvent *)event {
+    
+    
+//    FilelistViewController*vc=[FilelistViewController alloc]initView:<#(NSString *)#> withType:<#(NSString *)#>;
+//    
+//    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+@end
