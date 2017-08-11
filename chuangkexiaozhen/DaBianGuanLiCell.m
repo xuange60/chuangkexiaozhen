978935facf896960d4601b421f653f7e6fe56a01
xiaogao <11027688199@qq.com>
2017-08-11 16:57:03 +0800

    qqqqqqqqqqq

--- chuangkexiaozhen/DaBianGuanLiCell.m
+++ chuangkexiaozhen/DaBianGuanLiCell.m
@@ -16,9 +16,10 @@
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    
     [super setSelected:selected animated:animated];
 
-    // Configure the view for the selected state
+   
 }
 
 @end
