7f83e243670a975e925065293a7e192914b930e0
zhangxuan <734267682@qq.com>
2017-08-21 21:28:28 +0800

    111

--- chuangkexiaozhen/TuanDuiYunYing.h
+++ chuangkexiaozhen/TuanDuiYunYing.h
@@ -10,17 +10,24 @@
 
 @interface TuanDuiYunYing : CommNetWork
 
-
+//查询所有数据
 -(void)queryData:(NSString*)type;
 
+//管理员查询所有数据
 -(void)queryDataAdmin:(NSString*)type;
 
+//添加前查询参数
 -(void)queryParam:(NSString*)type;
 
+//添加数据
 -(void)addData:(NSDictionary*)param withType:(NSString*)type;
 
+//删除数据
 -(void)deleteData:(NSString*)ids withType:(NSString*)type;
 
+//查询记录详情
+-(void)queryDetail:(NSString*)ids withType:(NSString*)type;
 
-
+//审批记录
+-(void)shenpiData:(NSDictionary*)param withType:(NSString*)type;
 @end
